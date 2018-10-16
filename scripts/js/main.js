0d0d879b0de3c5b3e8f474a31043ff04b83dd5a8
M1nP <kuriputon224@yahoo.co.jp>
2018-10-16 21:30:07 +0900

    first ja

---
+++ scripts/js/main.js
@@ -0,0 +1,16 @@
+var dis = document.getElementById("wrap");
+var load = document.getElementById("loading");
+var back = document.getElementById("back");
+document.addEventListener("DOMContentLoaded",function(){
+    load.style.transitionDuration = "2.5s";
+    load.style.transform = "rotate(1000deg)";
+
+},false);
+window.onload = function(){
+    load.style.display = "none";
+    dis.style.display = "block";
+    back.style.overflow = "visible"
+};
+var day = new Date();
+var result = document.getElementById("daynow");
+result.textContent = day.toLocaleString();
